d0178b3c941adf0505db0d1dc671707987869a62
Raja Mukherji <rajamukherji@gmail.com>
2023-01-17 10:17:45 +0000

    Updates.

---
+++ src/ramp2.c
@@ -0,0 +1,161 @@
+#include "ramp2.h"
+
+#include <string.h>
+#include <stdatomic.h>
+
+typedef struct ramp2_page_t ramp2_page_t;
+
+struct ramp2_page_t {
+	ramp2_page_t *Next;
+	char Bytes[] __attribute__ ((aligned(16)));
+};
+
+struct ramp2_deferral_t {
+	ramp2_deferral_t *Next;
+	void (*Callback)(void *);
+	void *Arg;
+};
+
+struct ramp2_group_t {
+	ramp2_page_t * _Atomic Free;
+	size_t PageSize;
+};
+
+struct ramp2_t {
+	ramp2_group_t *Group;
+	ramp2_page_t *Page, *Full;
+	ramp2_deferral_t *Deferrals;
+	size_t PageSize, Space;
+};
+
+static inline ramp2_page_t *ramp2_page_new(size_t PageSize) {
+	ramp2_page_t *Page = (ramp2_page_t *)malloc(sizeof(ramp2_page_t) + PageSize);
+	return Page;
+}
+
+static inline void ramp2_page_free(ramp2_page_t *Page) {
+	free(Page);
+}
+
+ramp2_group_t *ramp2_group_new(size_t PageSize) {
+	PageSize += 15;
+	PageSize &= ~15;
+	ramp2_group_t *Group = (ramp2_group_t *)malloc(sizeof(ramp2_group_t));
+	Group->PageSize = PageSize;
+	return Group;
+}
+
+void ramp2_group_reset(ramp2_group_t *Group) {
+	ramp2_page_t *Old = atomic_exchange(&Group->Free, NULL);
+	for (ramp2_page_t *Page = Old->Next, *Next; Page; Page = Next) {
+		Next = Page->Next;
+		ramp2_page_free(Page);
+	}
+}
+
+void ramp2_group_free(ramp2_group_t *Group) {
+	ramp2_page_t *Page = Group->Free;
+	while (Page) {
+		ramp2_page_t *Next = Page->Next;
+		free(Page);
+		Page = Next;
+	}
+	free(Group);
+}
+
+static inline ramp2_page_t *ramp2_group_page_new(ramp2_group_t *Group) {
+	ramp2_page_t *Page = Group->Free, *Free;
+	do {
+		if (!Page) {
+			Page = ramp2_page_new(Group->PageSize);
+			break;
+		}
+		Free = Page->Next;
+	} while (!atomic_compare_exchange_weak(&Group->Free, &Page, Free));
+	Page->Next = NULL;
+	return Page;
+}
+
+ramp2_t *ramp2_new(ramp2_group_t *Group) {
+	ramp2_t *Ramp = (ramp2_t *)malloc(sizeof(ramp2_t));
+	Ramp->Group = Group;
+	Ramp->Page = ramp2_group_page_new(Group);
+	Ramp->Full = NULL;
+	Ramp->Deferrals = NULL;
+	Ramp->PageSize = Group->PageSize;
+	Ramp->Space = Group->PageSize;
+	return Ramp;
+}
+
+#define likely(x)    __builtin_expect (!!(x), 1)
+#define unlikely(x)  __builtin_expect (!!(x), 0)
+
+void *ramp2_alloc(ramp2_t *Ramp, size_t Size) {
+	Size += 15;
+	Size &= ~15;
+	if (likely(Size <= Ramp->Space)) {
+		Ramp->Space -= Size;
+		return Ramp->Page->Bytes + Ramp->Space;
+	} else if (Size <= Ramp->PageSize) {
+		ramp2_page_t *Old = Ramp->Page;
+		Old->Next = Ramp->Full;
+		Ramp->Full = Old;
+		ramp2_page_t *New = Ramp->Page = ramp2_group_page_new(Ramp->Group);
+		Ramp->Space = Ramp->PageSize - Size;
+		return New->Bytes + Ramp->Space;
+	} else {
+		void *Bytes = malloc(Size);
+		ramp2_defer(Ramp, free, Bytes);
+		return Bytes;
+	}
+}
+
+void *ramp2_strdup(ramp2_t *Ramp, const char *String) {
+	size_t Length = strlen(String);
+	char *Copy = ramp2_alloc(Ramp, Length + 1);
+	memcpy(Copy, String, Length);
+	Copy[Length] = 0;
+	return Copy;
+}
+
+ramp2_deferral_t *ramp2_defer(ramp2_t *Ramp, void (*Callback)(void *), void *Arg) {
+	ramp2_deferral_t *Deferral = (ramp2_deferral_t *)ramp2_alloc(Ramp, sizeof(ramp2_deferral_t));
+	Deferral->Next = Ramp->Deferrals;
+	Deferral->Callback = Callback;
+	Deferral->Arg = Arg;
+	Ramp->Deferrals = Deferral;
+	return Deferral;
+}
+
+static void ramp2_defer_nop(void *Arg) {}
+
+void ramp2_cancel(ramp2_deferral_t *Deferral) {
+	Deferral->Callback = ramp2_defer_nop;
+}
+
+void ramp2_clear(ramp2_t *Ramp) {
+	for (ramp2_deferral_t *Deferral = Ramp->Deferrals; Deferral; Deferral = Deferral->Next) Deferral->Callback(Deferral->Arg);
+	Ramp->Deferrals = NULL;
+	if (Ramp->Full) {
+		ramp2_page_t *First = Ramp->Full, *Last = First;
+		while (Last->Next) Last = Last->Next;
+		ramp2_group_t *Group = Ramp->Group;
+		ramp2_page_t *Free = Group->Free;
+		do {
+			Last->Next = Free;
+		} while (!atomic_compare_exchange_weak(&Group->Free, &Free, First));
+		Ramp->Full = NULL;
+	}
+	Ramp->Space = Ramp->PageSize;
+}
+
+void ramp2_free(ramp2_t *Ramp) {
+	ramp2_clear(Ramp);
+	ramp2_page_t *Page = Ramp->Page;
+	ramp2_group_t *Group = Ramp->Group;
+	ramp2_page_t *Free = Group->Free;
+	do {
+		Page->Next = Free;
+	} while (!atomic_compare_exchange_weak(&Group->Free, &Free, Page));
+	free(Ramp);
+}
---
+++ src/ramp2.h
@@ -0,0 +1,86 @@
+#ifndef RAMP2_H
+#define RAMP2_H
+
+#include <stdlib.h>
+
+typedef struct ramp2_t ramp2_t;
+typedef struct ramp2_group_t ramp2_group_t;
+
+/**
+ * \brief allocates a new ramp2_group_t object.
+ *
+ * \param PageSize size of a new page.
+ */
+ramp2_group_t *ramp2_group_new(size_t PageSize);
+
+/**
+ * \brief frees memory allocated within ramp2_group_t instance and frees memory blocks.
+ *
+ * \param Group ramp2_group_t object allocated with ramp2_group_new.
+ */
+void ramp2_group_reset(ramp2_group_t *Group);
+
+/**
+ * \brief frees a ramp2_group_t object and all allocated pages, individual ramp2_t objects must be freed earlier.
+ *
+ * \param Group ramp2_group_t object allocated with ramp2_group_new.
+ */
+void ramp2_group_free(ramp2_group_t *Group);
+
+/**
+ * \brief allocates a new ramp2_t object.
+ *
+ * \param Group group object for sharing free pages.
+ */
+ramp2_t *ramp2_new(ramp2_group_t *Group);
+
+/**
+ * \brief allocates a block of memory within ramp2_t instance.
+ *
+ * \param Ramp ramp2_t object allocated with ramp2_new.
+ * \param Size size of memory block to allocate.
+ */
+void *ramp2_alloc(ramp2_t *Ramp, size_t Size) __attribute__((malloc));
+
+/**
+ * \brief copies a string into a ramp2_t instance.
+ *
+ * \param Ramp ramp2_t object allocated with ramp2_new.
+ * \param String string to copy.
+ */
+void *ramp2_strdup(ramp2_t *Ramp, const char *String) __attribute__((malloc));
+
+typedef struct ramp2_deferral_t ramp2_deferral_t;
+
+/**
+ * \brief defers a function call until ramp2_clear or ramp2_reset.
+ *
+ * \param Ramp ramp2_t object allocated with ramp2_new.
+ * \param Function function to call on reset.
+ * \param Arg argument to pass to CleanupFn.
+ * \return A deferral reference which can be used to cancel this deferral.
+ */
+ramp2_deferral_t *ramp2_defer(ramp2_t *Ramp, void (*Function)(void *), void *Arg);
+
+/**
+ * \brief cancels a deferred call.
+ *
+ * \param Deferral ramp2_deferral_t returned by ramp2_defer.
+ */
+void ramp2_cancel(ramp2_deferral_t *Deferral);
+
+/**
+ * \brief frees memory allocated within ramp2_t instance while keeping memory blocks for reuse.
+ *
+ * \param Ramp ramp2_t object allocated with ramp2_new.
+ */
+void ramp2_clear(ramp2_t *Ramp);
+
+/**
+ * \brief frees a ramp2_t object and all allocated pages.
+ *
+ * \param Ramp ramp2_t object allocated with ramp2_new.
+ */
+void ramp2_free(ramp2_t *Ramp);
+
+#endif
